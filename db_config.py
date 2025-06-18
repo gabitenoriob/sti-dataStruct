d4f3df7f0039ed78693f854be1dd90c1f84ead34
gabitenoriob <gabitenoriob@gmail.com>
2025-06-18 12:19:19 -0300

    db config

--- db_config.py
+++ db_config.py
@@ -1,11 +1,30 @@
 from sqlalchemy import create_engine, Column, Integer, String, Text, ForeignKey, Date, Enum
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
 
-Base = declarative_base()
-PASSWORD = "amigosdalais123"
-DATABASE_URL = f"postgresql+psycopg2://postgres:{PASSWORD}@db.jtayehmyvemcbbcvpwxb.supabase.co:5432/postgres?client_encoding=utf8"
+load_dotenv()
+
+USER = os.getenv("user")
+PASSWORD = os.getenv("password").strip()
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
+
+#DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+DATABASE_URL = "postgresql+psycopg2://postgres:amigosdalais123@db.jtayehmyvemcbbcvpwxb.supabase.co:5432/postgres?sslmode=require"
+
+
 engine = create_engine(DATABASE_URL)
 
+Base = declarative_base()
+
+try:
+    with engine.connect() as connection:
+        print("Connection successful!")
+except Exception as e:
+    print(f"Failed to connect: {e}")
 # Tabelas
 class CategoriaEstrutura(Base):
     __tablename__ = 'categoria_estrutura'
