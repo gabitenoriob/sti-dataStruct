d257e000f7b4fd6e58ff18d0c47fdb61e6cb7579
Laís Souza <laissouza.aalmeida@gmail.com>
2025-07-03 14:48:39 -0300

    pontuacao em ingles e pt

--- utils/pontuacao.py
+++ utils/pontuacao.py
@@ -1,17 +1,30 @@
-#new file 
-
 def calcular_pontuacao(exercicio):
-    nivel = exercicio.nivel_dificuldade.lower()
-    return {
+    """
+    Retorna a pontuação com base no nível de dificuldade.
+    Suporta nomes em português e inglês.
+    """
+    nivel = exercicio.nivel_dificuldade.strip().lower()
+
+    map_niveis = {
         "facil": 10,
+        "easy": 10,
         "medio": 20,
-        "dificil": 30
-    }.get(nivel, 10)
+        "médio": 20,
+        "medium": 20,
+        "dificil": 30,
+        "difícil": 30,
+        "hard": 30
+    }
+
+    return map_niveis.get(nivel, 10)
 
 
 def update_pontuacao(db, aluno, exercicio):
+    """
+    Atualiza a pontuação do aluno ao concluir um exercício.
+    """
     pontos = calcular_pontuacao(exercicio)
-    
+
     if aluno.pontuacao_total is None:
         aluno.pontuacao_total = 0
 
